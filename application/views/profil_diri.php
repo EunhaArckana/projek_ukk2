874f67e3c01564c4f2762ce96bfeff67404321f1
EunhaArckana <rmanps666@gmail.com>
2022-10-24 16:03:53 +0700

    push perst

---
+++ application/views/profil_diri.php
@@ -0,0 +1,40 @@
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Profil Diri</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Profil Diri</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header border-0">
+                        <div class="d-flex justify-content-between">
+                            <h3 class="card-title">Profil Diri</h3>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="d-flex">
+                            <div class="row">
+                                <div class="col-md-2 col-xs-12">
+                                    <img src="<?= base_url('assets/dist/img/arkana.jpg') ?>" class="img-thumbnail" alt="There is no picture">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
